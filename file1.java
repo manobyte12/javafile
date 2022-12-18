095d27ae757690f80920758b1b702bf2d4e38dbd
manobyte12 <113885236+manobyte12@users.noreply.github.com>
2022-12-18 18:09:07 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -2,3 +2,10 @@ import java.util.Scanner;
 import java.lang.System;
 class file1
 {
+  public static void main(String  args[])
+  {
+    int a=5, b=300, c=30, d;
+    d=a+b+b;
+    System.out.prinln("sum="+d);
+  }
+}
